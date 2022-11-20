8fd11b50bf387695f51bd75166839d2d0591f5b1
ewerestr <ewerestr@gmail.com>
2022-11-20 21:05:39 +0500

    Restored a small part code from base project

--- EWX ICQBotAPI/ICQBot.cs
+++ EWX ICQBotAPI/ICQBot.cs
@@ -1,13 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Web;
 
 namespace EWXICQBotAPI
 {
     public class ICQBot
     {
+        ////    Main fields to work with API
+        private string _botApiKey;
+        private const string _botApiUrl = "https://api.icq.net/bot/v1/";
+
+        ////    Main bot constructor
+        public ICQBot(string token)
+        {
+            _botApiKey = token;
+        }
+
+        ////    API methods block 
+        //  Self
+        public string APISelfGet()
+        {
+            string response = SendRequest(_botApiUrl + "self/get?token=" + _botApiKey);
+            //convert to obj;
+            return null;
+        }
+        //  Messages
+        public string APIMessagesSendText(string chatId, string text) // just text without formats, attachments and others
+        {
+            string response = SendRequest(_botApiUrl + "messages/sendText?text=" + HttpUtility.UrlEncode(text) + "&token=" + _botApiKey);
+            //convert to obj;
+            return null;
+        }
+        
+        /* 
+         * Here will be methods for message generator soon
+         * Messages.SendFile    (GET,POST)
+         * Messages.SendVoice   (GET,POST)
+         */ 
+        
+        public string APIMessagesEditText(string chatId, int msgId, string text, string inlineKeyboardMarkup = null)
+        {
+            string response = SendRequest(_botApiUrl + "messages/sendText?text=" + HttpUtility.UrlEncode(text) + "&token=" + _botApiKey);
+            //convert to obj;
+            return null;
+        }
+
+
+        ////    Network interract methods block
+        public string SendRequest(string url)
+        {
+            Thread.Sleep(100);
+            string web_response;
+            try
+            {
+                WebRequest request = WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    web_response = reader.ReadToEnd();
+                }
+                response.Close();
+            }
+            catch (WebException e)
+            {
+                //# Print("Method :: sendRequest :: Exception: " + e.Message + "; ServerResponse: " + e.Response, true, true);
+                System.Console.WriteLine(e.StackTrace);
+                return e.Message;
+            }
+            return web_response;
+        }
+
+        ////    Utils methods block
+
 
     }
+
+    
 }
---
+++ EWX ICQBotAPI/RequestGenerator.cs
@@ -0,0 +1,158 @@
+﻿using System.Web;
+
+namespace EWXICQBotAPI
+{
+    public class EWXRequestGenerator
+    {
+        private string _icqToken;
+        private string _icqApi;
+
+        public EWXRequestGenerator(string icqToken, string icqApi)
+        {
+            _icqToken = icqToken;
+            _icqApi = icqApi;
+        }
+
+        public string ICQSelfGet()
+        {
+            string url = _icqApi + "self/get?token=" + _icqToken;
+            return url;
+        }
+
+        public string ICQMessagesSendText(string chatId, string text)
+        {
+            string url = _icqApi + "messages/sendText?token=" + _icqToken + "&chatId=" + chatId + "&text=" + HttpUtility.UrlEncode(text);
+            return url;
+        }
+
+        public string ICQMessagesSendFile(string chatId, string fileId) // deprecated
+        {
+            string url = _icqApi;
+            return url;
+        }
+
+        public string ICQMessagesSendVoice(string chatId, string fileId) // deprecated
+        {
+            string url = _icqApi;
+            return url;
+        }
+
+        public string ICQMessagesEditText(string chatId, string msgId, string text) // deprecated
+        {
+            string url = _icqApi;
+            return url;
+        }
+
+        public string ICQMessagesDeleteMessages(string chatId, string msgId) // deprecated
+        {
+            string url = _icqApi;
+            return url;
+        }
+
+        public string ICQMessagesAnswerCallbackQuery(string chatId) // deprecated
+        {
+            string url = _icqApi;
+            return url;
+        }
+
+        public string ICQChatsMembersDelete(string chatId, string members) // deprecated
+        {
+            string url = _icqApi;
+            return url;
+        }
+
+        // chats avatar set
+
+        public string ICQChatsSendActions(string chatId, string actions) // deprecated
+        {
+            string url = _icqApi;
+            return url;
+        }
+
+        public string ICQChatsGetInfo(string chatId)
+        {
+            string url = _icqApi + "chats/getInfo?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ICQChatsGetAdmins(string chatId)
+        {
+            string url = _icqApi + "chats/getAdmins?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ICQChatsGetMembers(string chatId, string cursor = null)
+        {
+            string url = _icqApi + "chats/getMembers?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ICQChatsGetBlockedUsers(string chatId)
+        {
+            string url = _icqApi + "chats/getBlockedUsers?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ICQChatsGetPendingUsers(string chatId)
+        {
+            string url = _icqApi + "chats/getPendingUsers?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ChatsBlockUser(string chatId) // deprecated
+        {
+            string url = _icqApi + "chats/getInfo?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ChatsUnblockUser(string chatId) // deprecated
+        {
+            string url = _icqApi + "chats/getInfo?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ChatsResolvePending(string chatId) // deprecated
+        {
+            string url = _icqApi + "chats/getInfo?token=" + _icqToken + "&chatId=" + chatId;
+            return url;
+        }
+
+        public string ChatsSetTitle(string chatId, string title)
+        {
+            string url = _icqApi + "chats/setTitle?token=" + _icqToken + "&chatId=" + chatId + "&title=" + title;
+            return url;
+        }
+
+        public string ChatsSetAbout(string chatId, string about)
+        {
+            string url = _icqApi + "chats/setAbout?token=" + _icqToken + "&chatId=" + chatId + "&about=" + about;
+            return url;
+        }
+
+        public string ChatsSetRules(string chatId, string rules)
+        {
+            string url = _icqApi + "chats/setRules?token=" + _icqToken + "&chatId=" + chatId + "&about=" + rules;
+            return url;
+        }
+
+        public string ChatsPinMessage(string chatId, int msgId)
+        {
+            string url = _icqApi + "chats/pinMessage?token=" + _icqToken + "&chatId=" + chatId + "&msgId=" + msgId;
+            return url;
+        }
+
+        public string ChatsUnpinMessage(string chatId, int msgId)
+        {
+            string url = _icqApi + "chats/unpinMessage?token=" + _icqToken + "&chatId=" + chatId + "&msgId=" + msgId;
+            return url;
+        }
+
+        public string FilesGetInfo(string fileId)
+        {
+            string url = _icqApi + "files/getInfo?token=" + _icqToken + "&fileId=" + fileId;
+            return url;
+        }
+
+        // events get ?!
+    }
+}
