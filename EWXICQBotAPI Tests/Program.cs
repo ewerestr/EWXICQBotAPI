5b4d1c2db5efe7c6d3c70dacce2d8aca73820697
ewerestr <ewerestr@gmail.com>
2022-11-22 01:05:34 +0500

    Added test template

--- EWXICQBotAPI Tests/Program.cs
+++ EWXICQBotAPI Tests/Program.cs
@@ -11,6 +11,60 @@ namespace EWXICQBotAPI_Tests
         static void Main(string[] args)
         {
 
+
+            ////    Basic tests, test that API methods works
+
+            //  Test /self/get
+
+            //  Test /messages/sendText
+
+            //  Test /messages/sendFile
+
+            //  Test /messages/sendVoice
+
+            //  Test /messages/editText
+
+            //  Test /messages/deleteMessages
+
+            //  Test /messages/answerCallbackQuery
+
+            //  Test /chats/members/delete
+
+            //  Test /chats/avatar/set
+
+            //  Test /chats/sendActions
+
+            //  Test /chats/getInfo
+
+            //  Test /chats/getAdmins
+
+            //  Test /chats/getMembers
+
+            //  Test /chats/getBlockedUsers
+
+            //  Test /chats/getPendingUsers
+
+            //  Test /chats/blockUser
+
+            //  Test /chats/unblockUser
+
+            //  Test /chats/resolvePending
+
+            //  Test /chats/setTitle
+
+            //  Test /chats/setAbout
+
+            //  Test /chats/setRules
+
+            //  Test /chats/pinMessage
+
+            //  Test /chats/unpinMessage
+
+            //  Test /files/getInfo
+
+            //  Test /events/get
+
+            ////    Global test
         }
     }
 }
